3eee71710d05b56239fad70fb02c0925719a9fd0
HermanoCastro65 <hermanocastro18@gmail.com>
2022-09-15 13:57:36 -0300

    delete structs.h file and put in produtos.h file

--- produtos.h
+++ produtos.h
@@ -1,8 +1,61 @@
 #ifndef PRODUTOS_H
 #define PRODUTOS_H
 
-#include "structs.h"
+// Struct de produto
+typedef struct produto
+{
+	char descricao[100];
+	int codigo, qtd_estoque;
+	float preco;
+} Produtos, *PProdutos, **PPProdutos;
 
+// Aloca o vetor de produtos
+PPProdutos aloca_vetor()
+{
+	PPProdutos produtos = (PPProdutos)malloc(sizeof(PProdutos));
+	if (produtos == NULL)
+	{
+		printf("\n\nMemória insuficiente\n\n");
+		exit(1);
+	}
+	return produtos;
+}
+
+// Aloca struct de produto
+void aloca_produto(PPProdutos produtos, int tamanho)
+{
+	produtos[tamanho] = (PProdutos)malloc(sizeof(Produtos));
+	if (produtos[tamanho] == NULL)
+	{
+		printf("\n\nMemória insuficiente\n\n");
+		exit(1);
+	}
+}
+
+// Encontra produto pelo código
+PProdutos get_produto(PPProdutos produtos, int codigo, int tamanho)
+{
+	int i;
+	for (i = 0; i < tamanho; i++)
+	{
+		if (codigo == produtos[i]->codigo)
+			return produtos[i];	
+	}
+	return NULL;
+}
+
+// Encontra posição do produto no vetor
+ int get_posicao(PPProdutos produtos, PProdutos produto, int tamanho)
+ {
+ 	int i;
+	for (i = 0; i < tamanho; i++)
+	{
+		if (produto == produtos[i])
+			return i;	
+	}
+	return;
+ }
+ 
 // Função incluir produtos
 void incluir_produtos(PPProdutos produtos, int tamanho)
 {
@@ -20,9 +73,10 @@ void incluir_produtos(PPProdutos produtos, int tamanho)
 		printf("Informe a quantidade em estoque do Produto: ");
 		scanf("%d", &produtos[tamanho]->qtd_estoque);
 		printf("Informe o preço do Produto: ");
-		scanf("%f", &produtos[tamanho]->preco);
-		printf("\nCONCLUÍDO \n");
+		scanf("%f", &produtos[tamanho]->preco)
 	}
+	
+	printf("\nCONCLUÍDO \n");
 }
 
 // Função alterar produtos
@@ -38,10 +92,11 @@ void alterar_produtos(PProdutos produto)
 	scanf("%d", &produto->qtd_estoque);
 	printf("Alterar o preço do Produto: ");
 	scanf("%f", &produto->preco);
+	
 	printf("\nCONCLUÍDO \n");
 }
 
-// Função excluir produto
+// Função excluir produtos
 void excluir_produtos(PPProdutos produtos, int posicao, int tamanho)
 {
 	system("cls");
@@ -55,6 +110,7 @@ void excluir_produtos(PPProdutos produtos, int posicao, int tamanho)
 	
 	produtos[tamanho] = NULL;
 	free(produtos[tamanho]);
+	
 	printf("\nCONCLUÍDO \n");
 }
 
@@ -68,6 +124,7 @@ void consultar_produtos(PProdutos produto)
 	printf("\n Descrição: %s", produto->descricao);
 	printf("\n Preço: R$ %.2f", produto->preco);
 	printf("\n Quantidade em Estoque: %d\n", produto->qtd_estoque);
+	
 	printf("\nCONCLUÍDO \n");
 }
 
@@ -83,6 +140,7 @@ void listar_produtos(PPProdutos produtos, int tamanho)
 		printf("\n Código: %d", produtos[i]->codigo);
 		printf("\n Descrição: %s", produtos[i]->descricao);
 	}
+	
 	printf("\nCONCLUÍDO \n");
 }
 
