eeb9f2169c095a7524e166a100569a0372217e56
HermanoCastro65 <hermanocastro18@gmail.com>
2022-09-20 22:16:10 -0300

    fix

--- menus.h
+++ menus.h
@@ -8,7 +8,7 @@ void menu_produtos(PPProdutos produtos, int *tamanho)
 {
 	int opcao, sair = 0, codigo;
 	PProdutos produto;
-	
+
 	// Menu de produtos
 	do
 	{
@@ -33,7 +33,7 @@ void menu_produtos(PPProdutos produtos, int *tamanho)
 			getch();
 			break;
 		case 2:
-			// Chama as funções de listar e alterar produtos
+			// Chama as função de listar e alterar produtos
 			listar_produtos(produtos, *tamanho);
 			printf("\nDigite o código do produto que deseja alterar: ");
 			scanf("%d", &codigo);
@@ -42,7 +42,7 @@ void menu_produtos(PPProdutos produtos, int *tamanho)
 			produto = get_produto(produtos, codigo, *tamanho);
 			if (produto)
 				alterar_produtos(produto);
-				
+
 			getch();
 			break;
 		case 3:
@@ -61,7 +61,7 @@ void menu_produtos(PPProdutos produtos, int *tamanho)
 				excluir_produtos(produtos, posicao, *tamanho);
 				*tamanho = *tamanho - 1;
 			}
-			
+
 			getch();
 			break;
 		case 4:
@@ -69,18 +69,18 @@ void menu_produtos(PPProdutos produtos, int *tamanho)
 			listar_produtos(produtos, *tamanho);
 			printf("\nDigite o código do produto que deseja consultar: ");
 			scanf("%d", &codigo);
-			
+
 			// Busca o produto pelo código
 			produto = get_produto(produtos, codigo, *tamanho);
 			if (produto)
 				consultar_produtos(produto);
-	
+
 			getch();
 			break;
 		case 5:
 			// Chama a função listar produtos
 			listar_produtos(produtos, *tamanho);
-			
+
 			getch();
 			break;
 		case 6:
@@ -95,17 +95,16 @@ void menu_produtos(PPProdutos produtos, int *tamanho)
 	} while (!sair);
 }
 
-
 void menu_pedidos(PPProdutos produtos, PPPedidos historico, int *tamanho, int *tam_hist)
-{			
-	//Aloca vetor para os produtos no pedido
+{
+	// Aloca vetor para os produtos no pedido
 	PPProdutos pedidos = aloca_vetor();
-	
-	if (pedidos) 
+
+	if (pedidos)
 	{
-		int opcao, sair = 0, codigo, tam = 0;	
+		int opcao, sair = 0, codigo, tam = 0;
 		PProdutos produto, pedido;
-	
+
 		// Menu de pedidos
 		do
 		{
@@ -114,19 +113,20 @@ void menu_pedidos(PPProdutos produtos, PPPedidos historico, int *tamanho, int *t
 			printf("\n(2) Consulta Pedidos");
 			printf("\n(3) Excluir Pedidos");
 			printf("\n(4) Alterar Pedidos");
-			printf("\n(5) Sair / Finalizar Pedidos");;
+			printf("\n(5) Sair / Finalizar Pedidos");
+			;
 			printf("\n\nDigite uma opção: ");
 			scanf("%d", &opcao);
-	
+
 			switch (opcao)
 			{
 			case 1:
 				// Chama as funções de listar produtos, consultar produtos e adicionar pedidos
 				listar_produtos(produtos, *tamanho);
-	
+
 				printf("\nDigite o código do produto que deseja adicionar: ");
 				scanf("%d", &codigo);
-				
+
 				// Busca o produto pelo código
 				produto = get_produto(produtos, codigo, *tamanho);
 				if (produto)
@@ -136,16 +136,16 @@ void menu_pedidos(PPProdutos produtos, PPPedidos historico, int *tamanho, int *t
 					adicionar_pedidos(pedidos, produto, tam);
 					tam = tam + 1;
 				}
-				
+
 				getch();
 				break;
 			case 2:
-				// Chama a função listar produtos e colsultar pedidos
+				// Chama as funções listar produtos e colsultar pedidos
 				listar_produtos(pedidos, tam);
-	
+
 				printf("\nDigite o código do produto que deseja consultar no pedido: ");
 				scanf("%d", &codigo);
-				
+
 				// Busca o produto pelo código
 				pedido = get_produto(pedidos, codigo, tam);
 				if (pedido)
@@ -154,16 +154,16 @@ void menu_pedidos(PPProdutos produtos, PPPedidos historico, int *tamanho, int *t
 					int posicao = get_posicao(pedidos, pedido, tam);
 					consultar_pedidos(pedido, posicao);
 				}
-				
+
 				getch();
 				break;
 			case 3:
 				// Chama as funções de listar produtos,  repor pedidos e excluir produtos
 				listar_produtos(pedidos, tam);
-		
+
 				printf("\nDigite o código do produto que deseja excluir no pedido: ");
 				scanf("%d", &codigo);
-		
+
 				// Busca o produto pelo código
 				produto = get_produto(produtos, codigo, *tamanho);
 				pedido = get_produto(pedidos, codigo, tam);
@@ -175,7 +175,7 @@ void menu_pedidos(PPProdutos produtos, PPPedidos historico, int *tamanho, int *t
 					excluir_produtos(pedidos, posicao, tam);
 					tam = tam - 1;
 				}
-				
+
 				getch();
 				break;
 			case 4:
@@ -183,20 +183,18 @@ void menu_pedidos(PPProdutos produtos, PPPedidos historico, int *tamanho, int *t
 				listar_produtos(pedidos, tam);
 				printf("\nDigite o código do produto que deseja alterar no pedido: ");
 				scanf("%d", &codigo);
-		
+
 				// Busca o produto pelo código
 				produto = get_produto(produtos, codigo, *tamanho);
 				pedido = get_produto(pedidos, codigo, tam);
-				if (produto && pedido)
-					alterar_pedidos(produto, pedido);
-					
+				
 				getch();
 				break;
 			case 5:
 				// Chama as função finalizar pedido
 				finalizar_pedidos(pedidos, historico, tam, *tam_hist);
 				*tam_hist = *tam_hist + 1;
-	
+
 				getch();
 				sair = 1;
 				break;
@@ -205,7 +203,8 @@ void menu_pedidos(PPProdutos produtos, PPPedidos historico, int *tamanho, int *t
 				getch();
 				sair = 0;
 			}
-		} while (!sair);	
+		} while (!sair);
+		
 	}
 }
 
